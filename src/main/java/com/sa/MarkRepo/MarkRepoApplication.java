a66099e72a62e8855525ef06848b5cc0eade8d9f
user <shamalajames@gmail.com>
2018-03-06 11:52:35 +0530

    hhh

---
+++ src/main/java/com/sa/MarkRepo/MarkRepoApplication.java
@@ -0,0 +1,12 @@
+package com.sa.MarkRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkRepoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sa/MarkRepo/MarkRepoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sa.MarkRepo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarkRepoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
